4758ddc7577a4a729ae6b83059b68fb9fd337f36
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-02-26 20:00:29 -0500

    Persistencia de datos con Local Storage

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: 'Make la compra', completed: false },
-  { text: 'Make Dinner', completed: false },
-  { text: 'Make la limpieza', completed: false },
-]
+// const defaultTodos = [
+//   { text: 'Make la compra', completed: false },
+//   { text: 'Make Dinner', completed: false },
+//   { text: 'Make la limpieza', completed: false },
+// ]
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  // const [todos, setTodos] = React.useState(localStorageTodos ? JSON.parse(localStorageTodos) : []);
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -26,11 +36,18 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
@@ -38,6 +55,7 @@ function App() {
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
